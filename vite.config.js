d333dd6c15c7fb7999612fb662545a28052a2051
joe <josephdehazounde@gmail.com>
2026-03-28 16:45:02 +0100

    Reduce auth page size

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    host: true
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) {
+              return 'vendor-react'
+            }
+            if (id.includes('@fortawesome')) {
+              return 'vendor-icons'
+            }
+            return 'vendor'
+          }
+        }
+      }
+    }
+  }
 })
